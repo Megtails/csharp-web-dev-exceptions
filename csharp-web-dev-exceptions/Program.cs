5bf566a0dbd4a9650339fb1bf7e30ffacde82bcd
Meghan <stlmeghan@yahoo.com>
2022-09-22 23:13:53 -0500

    done I think

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,49 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
             // Write your code here!
+            if (y == 0.0)
+            {
+                throw ArgumentOutOfRangeException("y", "You cannot divide by 0!");
+            } else
+            {
+                return x / y;
+            }
+            
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
+            if (fileName==null || fileName == "")
+            {
+                throw ArgumentNullException("fileName", "Must pass through a file.");
+            }
+            else if (fileName[fileName.Length-1] == "s" && fileName[fileName.Length-2] == "c" && fileName[fileName.Length-3] == ".")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
-        */
+        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try 
+            {
+                Divide(9.9, 0.0)
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -29,6 +57,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Brad", "");
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
+
+            try
+            {
+                foreach(student s in students)
+                {
+                    CheckFileExtension(s);
+                }
+            }
+            catch(ArgumentNullException t)
+            {
+                Console.WriteLine(t.Message);
+            }
 
 
         }
